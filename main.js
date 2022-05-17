7b31309d68c1e8f2a700bf3ca64074f716e49ebc
NguyenThai73 <101828062+NguyenThai73@users.noreply.github.com>
2022-05-18 02:35:44 +0700

    commit

---
+++ main.js
@@ -0,0 +1,65 @@
+function gamingGear(){
+    document.getElementById('location').innerText=`GAMING GEAR`;
+    let a = document.getElementsByClassName('content__products1');
+    let b = document.getElementsByClassName('content__products2');
+    let c = document.getElementsByClassName('content__products3');
+    let d = document.getElementsByClassName('content__products4');
+    let e = document.getElementsByClassName('content__products5');
+    a[0].style.display ='flex';
+    b[0].style.display ='none';
+    c[0].style.display ='none';
+    d[0].style.display ='none';
+    e[0].style.display ='none';
+}
+function phuKienMt(){
+    document.getElementById('location').innerText=`PHỤ KIỆN MÁY TÍNH`;
+    let a = document.getElementsByClassName('content__products1');
+    let b = document.getElementsByClassName('content__products2');
+    let c = document.getElementsByClassName('content__products3');
+    let d = document.getElementsByClassName('content__products4');
+    let e = document.getElementsByClassName('content__products5');
+    a[0].style.display ='none';
+    b[0].style.display ='flex';
+    c[0].style.display ='none';
+    d[0].style.display ='none';
+    e[0].style.display ='none';
+}
+function moHinh(){
+    document.getElementById('location').innerText=`MÔ HÌNH`;
+    let a = document.getElementsByClassName('content__products1');
+    let b = document.getElementsByClassName('content__products2');
+    let c = document.getElementsByClassName('content__products3');
+    let d = document.getElementsByClassName('content__products4');
+    let e = document.getElementsByClassName('content__products5');
+    a[0].style.display ='none';
+    b[0].style.display ='none';
+    c[0].style.display ='flex';
+    d[0].style.display ='none';
+    e[0].style.display ='none';
+}   
+function banGheG() {
+    document.getElementById('location').innerText=`BÀN GHẾ GAMING`;
+    let a = document.getElementsByClassName('content__products1');
+    let b = document.getElementsByClassName('content__products2');
+    let c = document.getElementsByClassName('content__products3');
+    let d = document.getElementsByClassName('content__products4');
+    let e = document.getElementsByClassName('content__products5');
+    a[0].style.display ='none';
+    b[0].style.display ='none';
+    c[0].style.display ='none';
+    d[0].style.display ='flex';
+    e[0].style.display ='none';
+}
+function khac(){
+    document.getElementById('location').innerText=`SẢN PHẨM KHÁC`;
+    let a = document.getElementsByClassName('content__products1');
+    let b = document.getElementsByClassName('content__products2');
+    let c = document.getElementsByClassName('content__products3');
+    let d = document.getElementsByClassName('content__products4');
+    let e = document.getElementsByClassName('content__products5');
+    a[0].style.display ='none';
+    b[0].style.display ='none';
+    c[0].style.display ='none';
+    d[0].style.display ='none';
+    e[0].style.display ='flex';
+}
